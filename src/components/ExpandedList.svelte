5a3a41a1345d80ca44deec29db797153bbe526c2
chayapatr <chayapatr.arc@gmail.com>
2023-08-01 16:52:34 +0700

    feat: add expanded list

---
+++ src/components/ExpandedList.svelte
@@ -0,0 +1,22 @@
+<script>
+  import List from "./List.svelte";
+
+  export let title;
+  export let list;
+
+  let expand = false;
+</script>
+
+<div class="grid gap-3">
+  <div
+    class={`list-el hover:cursor-pointer ${expand && "font-medium"}`}
+    on:click={() => (expand = !expand)}
+  >
+    [{expand ? "-" : "+"}] {title} ({list.length}{" "} items)
+  </div>
+  <div class={`grid gap-5  mb-2 ${expand || "hidden"}`}>
+    {#each list as el}
+      <List l={"• " + el?.l} r1={el?.r1} r2={el?.r2} />
+    {/each}
+  </div>
+</div>
---
+++ src/components/List.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let l = "";
+  export let r1 = "";
+  export let r2 = "";
+</script>
+
+<a class="group list-el grid grid-cols-6 lg:grid-cols-10" href="">
+  <h3
+    class={`group-hover:font-medium text-lg ${
+      r1 ? "col-span-4 lg:col-span-8" : "col-span-5 lg:col-span-9"
+    }`}
+  >
+    {l}
+  </h3>
+  {#if r1}
+    <h3 class="group-hover:font-medium text-lg text-end">{r1}</h3>
+  {/if}
+  <h3 class="group-hover:font-medium text-lg text-end">
+    {r2}
+  </h3>
+</a>
