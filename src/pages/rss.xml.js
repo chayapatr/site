cb192ba4e9b31b954dec6bed491dc21bf761dac4
chayapatr <chayapatr.arc@gmail.com>
2024-01-02 02:56:31 +0700

    fix: rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-  const posts = await getCollection("s");
+  const posts = await getCollection("g");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
