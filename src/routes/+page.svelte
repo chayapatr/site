3c580b9d4b5647c00cc39e737af0af6028f8be14
chayapatr <chayapatr.arc@gmail.com>
2024-04-24 19:56:54 +0700

    feat: things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,105 +1,172 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { micromark } from 'micromark';
 
 	let s = 1;
 	let drag = false;
 	let g: any = () => {};
 
-	let els: { x: number; y: number; text: string }[] = [
-		{ x: 0, y: 0, text: 'test1' },
-		{ x: 20, y: 30, text: 'test2' }
+	let x = 0,
+		y = 0;
+
+	let els: { x: number; y: number; text: string; sx; sy }[] = [
+		{ x: 10, y: 10, text: 'test1', sx: '320', sy: '320' }
 	];
 
 	let cur: number = -1;
-	let two = (n: number) => {
+	const two = (n: number) => {
 		// convert to two digit float
 		return n.toFixed(2);
 	};
 
-	let file = '';
+	const getContent = async (slug: string) => {
+		const text = await fetch(slug).then((res) => res.text());
+
+		const slugReplacer = (match, slug) => {
+			return `<a onclick={update("${slug}")} href="?${slug}"`;
+		};
+
+		const styleParser = (text: string) => {
+			return text.replace(/<a href="\/([A-Za-z1-9\s-]*)(?:\.md)?"/g, slugReplacer);
+		};
+
+		return styleParser(micromark(text));
+	};
 
 	onMount(async () => {
+		window.update = async (s) => {
+			els = [
+				...els,
+				{
+					x: els[els.length - 1].x + 50,
+					y: els[els.length - 1].y + 50,
+					sx: '320',
+					sy: '320',
+					text: await getContent(`/${s}.md`)
+				}
+			];
+		};
+
 		window.addEventListener(
 			'wheel',
 			function (e) {
-				e.preventDefault();
-				console.log(e);
-				let d = s - 0.003 * e.deltaY;
-				const min = 0.6;
-				const max = 1.4;
-				s = d > min ? (d < max ? d : max) : min;
+				if (cur === -1) {
+					e.preventDefault();
+					let d = s - 0.003 * e.deltaY;
+					const min = 0.5;
+					const max = 1.5;
+					s = d > min ? (d < max ? d : max) : min;
+				}
 			},
 			{ passive: false }
 		);
 
 		g = (e: MouseEvent) => {
-			console.log('ff');
-			if (drag) {
+			if (drag && cur > -1) {
 				els[cur].x += e.movementX / s;
 				els[cur].y += e.movementY / s;
+			} else if (drag) {
+				x += 0.75 * e.movementX;
+				y += 0.75 * e.movementY;
 			}
 		};
 	});
 </script>
 
-<svelte:window on:mousemove={g} on:mouseup={() => (drag = false)} />
+<svelte:window on:mousemove={g} on:mouseup={() => ((drag = false), (cur = -1))} />
 
 <div class="fixed bottom-0 left-0 z-50 m-3 flex w-screen justify-center">
 	<div
-		class="flex min-w-60 items-center justify-between gap-2 rounded-full border border-neutral-700 bg-neutral-800 p-1 text-white shadow-sm"
+		class="glass flex min-w-60 items-center justify-between gap-2 rounded-full border border-neutral-800 p-1 text-white shadow-md"
 	>
-		<div class="ml-2">{s}</div>
-		<button
-			class="aspect-square w-7 rounded-full bg-neutral-700 text-white"
-			on:click={async () => {
-				els = [
-					...els,
-					{
-						x: els[els.length - 1].x + 50,
-						y: els[els.length - 1].y + 50,
-						text: await fetch(`/${els.length - 1}.md`).then((res) => res.text())
-					}
-				];
-			}}>+</button
-		>
+		<div class="ml-2 flex gap-1">
+			<div>{cur}</div>
+			<div>{two(s)}</div>
+		</div>
+		<div class="flex gap-1">
+			<button
+				class="aspect-square w-7 rounded-full bg-neutral-700 text-white"
+				on:click={async () => {
+					(x = 0), (y = 0), (s = 1);
+				}}>◻️</button
+			>
+			<button
+				class="aspect-square w-7 rounded-full bg-neutral-700 text-white"
+				on:click={async () => {
+					els = [
+						...els,
+						{
+							x: els.length > 0 ? els[els.length - 1].x + 50 : 0,
+							y: els.length > 0 ? els[els.length - 1].y + 50 : 0,
+							text: await getContent(`/!@$.md`),
+							sx: '320',
+							sy: '320'
+						}
+					];
+				}}>+</button
+			>
+		</div>
 	</div>
 </div>
 
-<div class="h-screen w-screen">
-	<div class="h-full w-full overflow-hidden">
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+	class={`h-screen w-screen ${drag ? 'mousedown' : ''}`}
+	on:mousedown={() => {
+		drag = true;
+	}}
+>
+	<div class={`h-full w-full overflow-hidden bg-neutral-950`}>
 		<div
 			class="relative min-h-full min-w-full"
 			style={`
-            transform: scale(${s});
+            transform: matrix(${s}, 0, 0, ${s}, ${x}, ${y});
         `}
 		>
 			{#each els as el, i}
 				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
 				<div
-					class="noselect absolute aspect-square w-36 p-1 text-xs text-white hover:cursor-grab"
-					on:mousedown={() => {
-						drag = true;
+					class={`noselect glass absolute aspect-square max-h-80 w-80 flex-col justify-between rounded-sm border border-neutral-800 text-xs text-white shadow-sm transition-shadow hover:cursor-grab hover:shadow-md
+                        ${cur === i ? 'border-3 overflow-scroll border-white/30' : 'overflow-hidden'}
+                    `}
+					on:click={() => {
 						cur = i;
+					}}
+					on:mousedown={() => {
+						cur = i;
+						drag = true;
 					}}
 					style={`
                         transform: matrix(1, 0, 0, 1, ${el.x}, ${el.y});
-                        background-color: hsl(${i * 20}, 100%, 40%);
                 `}
 				>
-					{drag && cur === i}
-					{two(el.x)}
-					{two(el.y)}
-					{i}
-					{el.text}
+					<div class="prose prose-sm prose-invert px-1 py-6">
+						{@html el.text}
+					</div>
 					<!-- svelte-ignore a11y-click-events-have-key-events -->
-					<button
-						class="absolute right-0 top-0 aspect-square w-6 border bg-red-400"
-						on:click={() => {
-							els = els.filter((_, j) => i !== j);
-						}}
-					>
-						x
-					</button>
+					<div class="absolute top-0 flex w-full justify-between p-1">
+						<div class="text-neutral-500">
+							{two(el.x)}
+							{two(el.y)}
+						</div>
+						<button
+							class=" text-neutral-500"
+							on:click={() => {
+								els = els.filter((_, j) => i !== j);
+							}}
+						>
+							[x]
+						</button>
+					</div>
+					<!-- <div class="fixed bottom-0 w-full p-1">
+						<div class="text-neutral-500">
+							{drag && cur === i}
+							{two(el.x)}
+							{two(el.y)}
+							{i}
+						</div>
+					</div> -->
 				</div>
 			{/each}
 		</div>
@@ -115,5 +182,15 @@
 		-ms-user-select: none; /* Internet Explorer/Edge */
 		user-select: none; /* Non-prefixed version, currently
                                   supported by Chrome, Edge, Opera and Firefox */
+	}
+	.mousedown {
+		cursor: grab;
+	}
+	.glass {
+		/* background: linear-gradient(135deg, rgba(100, 100, 100, 0.07), rgba(100, 100, 100, 0.15)); */
+		background: rgba(100, 100, 100, 0.1);
+		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+		backdrop-filter: blur(2px);
+		-webkit-backdrop-filter: blur(2px);
 	}
 </style>
