feca065e1cc83a897cd57068c540223a9cb0f02d
chayapatr <chayapatr.arc@gmail.com>
2024-12-18 14:30:02 +0800

    feat: /c

---
+++ src/routes/c/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		// redirect to "https://from.pub/?page=clipboard"
+		window.location.href = 'https://from.pub/?page=clipboard';
+	});
+</script>
